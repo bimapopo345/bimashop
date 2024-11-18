4787bf9f42e77918f49331e89a416c77da24d6aa
bimapopo345 <bimapopo345@gmail.com>
2024-11-18 18:04:46 +0700

    Setup full stack workflow: added product routes, nodemon, concurrently, environment variables, and initial product fetching functionality

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,12 +1,21 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import Rating from "../components/Rating";
 import { Row, Col, ListGroup, Image, Card, Button } from "react-bootstrap";
-import products from "../products";
+import axios from "axios";
 const ProductScreen = () => {
+  const [product, setProduct] = useState({});
+
   const { id: productId } = useParams();
-  const product = products.find((p) => p._id === productId);
-  console.log(product);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${productId}`);
+      setProduct(data);
+    };
+    fetchProduct();
+  }, [productId]);
   return (
     <>
       <Link to="/" className="btn btn-light my-3">
